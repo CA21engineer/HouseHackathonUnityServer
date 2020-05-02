84f886b9d54459c724328cd5e38468800cfb8bb8
Suzuki Takeo <takeo@stko.info>
2020-05-02 14:21:22 +0900

    fix docker build

--- deploy.sh
+++ deploy.sh
@@ -23,18 +23,14 @@ fi
 if type "sbt" > /dev/null 2>&1; then
   sbt clean docker:stage
 else
-  if type "docker image inspect hseeberger/scala-sbt:8u222_1.3.10_2.12.8" > /dev/null 2>&1; then
-    # あった場合は何もしない
-  eles
-    docker build \
-      --build-arg BASE_IMAGE_TAG="8u212-b04-jdk-stretch" \
-      --build-arg SBT_VERSION="1.3.10" \
-      --build-arg SCALA_VERSION="2.12.8" \
-      --build-arg USER_ID=1001 \
-      --build-arg GROUP_ID=1001 \
-      -t hseeberger/scala-sbt:8u222_1.3.10_2.12.8 \
-      github.com/hseeberger/scala-sbt.git#:debian
-  fi
+  docker build \
+    --build-arg BASE_IMAGE_TAG="8u212-b04-jdk-stretch" \
+    --build-arg SBT_VERSION="1.3.10" \
+    --build-arg SCALA_VERSION="2.12.8" \
+    --build-arg USER_ID=1001 \
+    --build-arg GROUP_ID=1001 \
+    -t hseeberger/scala-sbt:8u222_1.3.10_2.12.8 \
+    github.com/hseeberger/scala-sbt.git#:debian
 
   docker run \
   --rm -v /var/run/docker.sock:/var/run/docker.sock \
