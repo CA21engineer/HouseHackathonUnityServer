be12a4e0e4a14f93d529f1f48df080153f1773e6
Suzuki Takeo <takeo@stko.info>
2020-05-05 17:50:17 +0900

    refactor sh

--- run-local.sh
+++ run-local.sh
@@ -23,11 +23,11 @@ fi
 
 
 if type "docker-compose" > /dev/null 2>&1; then
-  docker-compose -f docker-compose.yaml up --build -d
+  docker-compose -f docker-compose-local.yaml up --build -d
 else
   docker run \
   --rm -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$PWD:/$PWD" -w="/$PWD" \
   docker/compose:1.22.0 \
-  -f docker-compose.yaml up --build -d
+  -f docker-compose-local.yaml up --build -d
 fi
